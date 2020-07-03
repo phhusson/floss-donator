9db04f87650f99b20bc0bc59e61958a136c45b33
Pierre-Hugues Husson <phh@phh.me>
2020-07-03 19:00:15 +0200

    Show a progress bar representing how much apps deserve money

--- app/src/main/java/me/phh/flossdonator/MainActivity.kt
+++ app/src/main/java/me/phh/flossdonator/MainActivity.kt
@@ -2,17 +2,20 @@ package me.phh.flossdonator
 
 import android.app.usage.UsageStatsManager
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.JsonReader
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONArray
 import java.util.*
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,55 +23,161 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+    }
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+    data class AppFlags(
+        val additionalFeatures: Boolean = false,
+        val reducedFeatures: Boolean = false,
+        val externalContent: Boolean = false,
+        val instantMessaging: Boolean = false,
+        val usefulInBackground: Boolean = false)
+    data class AppLocalInfo(
+        val foregroundTime: Long = 0L,
+        val installer: String = "")
+
+    data class AppInfo(
+        val flags: AppFlags,
+        val localInfo: AppLocalInfo
+    )
+
+
+    fun refreshStats() {
+        val usageManager = getSystemService(UsageStatsManager::class.java)!!
+
+        val now = Date()
+
+        val cal = Calendar.getInstance()
+        cal.set(1900 + now.year, now.month, 0)
+
+        val firstThisMonth = cal.time
+        if(now.month == 0)
+            cal.set(1900 + now.year-1, 11, 0)
+        else
+            cal.set(1900 + now.year, now.month-1, 0)
+        val firstPreviousMonth = cal.time
+
+        Log.e("FLOSS-Donator", "Asking for stats from ${firstPreviousMonth.time} to ${firstThisMonth.time}")
+
+        val listOfApps = HashMap<String,AppInfo>()
+
+        val stats = usageManager.queryAndAggregateUsageStats(firstPreviousMonth.time, firstThisMonth.time)
+        resources.openRawResource(R.raw.pkgs).use {
+            val jsonParser = JsonReader(it.bufferedReader())
+            jsonParser.beginArray()
+            while(jsonParser.hasNext()) {
+                var pkgId: String? = null
+                var flags = AppFlags()
+
+                jsonParser.beginObject()
+                while(jsonParser.hasNext()) {
+                    val attrName = jsonParser.nextName()
+                    if(attrName == "pkgId") {
+                        pkgId = jsonParser.nextString()
+                    } else if(attrName == "flags") {
+                        jsonParser.beginObject()
+                        while(jsonParser.hasNext()) {
+                            val flagName = jsonParser.nextName()
+                            val value = jsonParser.nextBoolean()
+                            flags = when(flagName) {
+                                "external_content" -> flags.copy(externalContent = value)
+                                "instant_messaging" -> flags.copy(instantMessaging = value)
+                                "reduced_features" -> flags.copy(reducedFeatures = value)
+                                "additional_features" -> flags.copy(additionalFeatures = value)
+                                "useful_in_background" -> flags.copy(usefulInBackground = value)
+                                else -> flags
+                            }
+                        }
+                        jsonParser.endObject()
+                    } else {
+                        jsonParser.skipValue()
+                    }
+                }
+                jsonParser.endObject()
+
+                if(pkgId == null) continue
+                if(!stats.containsKey(pkgId)) continue
+
+                val installer = try { packageManager.getInstallerPackageName(pkgId) } catch(e: Exception) { "" } ?: ""
+                val localInfo = AppLocalInfo(
+                    stats[pkgId]!!.totalTimeInForeground,
+                    installer
+                )
+                listOfApps[pkgId] = AppInfo(flags, localInfo)
+
+                Log.e("FLOSS-Donator", "$pkgId has been used ${stats[pkgId]!!.totalTimeInForeground}, installer is $installer")
+            }
+            jsonParser.endArray()
+        }
+        runOnUiThread {
+            refreshScreen(listOfApps)
         }
     }
+
+    fun refreshScreen(list: Map<String, AppInfo>) {
+        val listWidget = findViewById<LinearLayout>(R.id.listOfApps)
+        listWidget.removeAllViews()
+
+        val nBackgrounds = list.count { it.value.flags.usefulInBackground }
+        //Allocate somewhere between 10% and 30% for all the background apps
+        val allocatedToBackgrounds = if(nBackgrounds > 0) 10+20*(1-Math.exp( (1 - nBackgrounds )/5.0)) else 0.0
+
+        Log.e("FLOSS-Donator", "Let's allocate $allocatedToBackgrounds to $nBackgrounds apps")
+        //We don't count apps that are useful in background, because they already got their share
+        val totalForegroundTime =
+            list
+                .map { it.value }
+                .filter { !it.flags.usefulInBackground }
+                .map { it.localInfo.foregroundTime }
+                .sum().toDouble()
+
+        for( (app,info) in list) {
+            try {
+                val percent = if(info.flags.usefulInBackground) allocatedToBackgrounds/nBackgrounds else 100.0 * info.localInfo.foregroundTime / totalForegroundTime
+                val layout = layoutInflater.inflate(R.layout.app_row, listWidget, false)
+
+                val iconWidget = layout.findViewById<ImageView>(R.id.app_icon)
+                val textWidget = layout.findViewById<TextView>(R.id.app_name)
+                val usageWidget = layout.findViewById<ProgressBar>(R.id.usage_percent)
+
+                usageWidget.progress = percent.toInt()
+
+                val icon = packageManager.getApplicationIcon(app)
+                iconWidget.setImageDrawable(icon)
+
+                val appPmInfo = packageManager.getApplicationInfo(app, 0)
+                val appLabelId = appPmInfo.labelRes
+                val appLabel =
+                    if (appLabelId == 0) appPmInfo.nonLocalizedLabel else packageManager.getResourcesForApplication(
+                        app
+                    ).getString(appLabelId)
+
+                textWidget.text = appLabel
+                listWidget.addView(layout)
+            } catch(e: Exception) {
+                Log.d("FLOSS-Donator", "Failed adding info for $app", e)
+            }
+        }
+    }
+
+    val processingPackagesHandler = {
+        val thread = HandlerThread("Processing Packages Handler")
+        thread.start()
+        Handler(thread.looper)
+    }()
 
     override fun onResume() {
         super.onResume()
 
-        thread {
-            val usageManager = getSystemService(UsageStatsManager::class.java)
+        /*
+         * This code doesn't work, checkSelfPermission returns -1 when permission has been given?!?
+        if(checkSelfPermission(Manifest.permission.PACKAGE_USAGE_STATS) != PackageManager.PERMISSION_GRANTED) {
+            Log.e("FLOSS-Donator", "I'm missing package usage stats permission")
+            startActivity(Intent("android.settings.USAGE_ACCESS_SETTINGS"))
+            return
+        } */
 
-            val now = Date()
-            val cal = Calendar.getInstance()
-            cal.set(now.year, now.month, 0)
-
-            val firstThisMonth = cal.time
-            if(now.month == 0)
-                cal.set(now.year-1, 11, 0)
-            else
-                cal.set(now.year, now.month-1, 0)
-            val firstPreviousMonth = cal.time
-
-            val stats = usageManager.queryAndAggregateUsageStats(firstPreviousMonth.time, firstPreviousMonth.time)
-            resources.openRawResource(R.raw.pkgs).use {
-                val jsonParser = JsonReader(it.bufferedReader())
-                jsonParser.beginArray()
-                while(jsonParser.hasNext()) {
-                    var pkgId: String? = null
-
-                    jsonParser.beginObject()
-                    while(jsonParser.hasNext()) {
-                        val attrName = jsonParser.nextName()
-                        if(attrName == "pkgId") {
-                            pkgId = jsonParser.nextString()
-                        } else {
-                            jsonParser.skipValue()
-                        }
-                    }
-                    jsonParser.endObject()
-
-                    if(pkgId == null) continue
-                    if(!stats.containsKey(pkgId)) continue
-
-                    Log.e("FLOSS-Donator", "$pkgId has been used ${stats[pkgId]!!.totalTimeInForeground}")
-                }
-                jsonParser.endArray()
-            }
+        processingPackagesHandler.post {
+            refreshStats()
         }
     }
 
